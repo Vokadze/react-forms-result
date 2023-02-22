3df2767587dbb0e60d3eca1b2615fae232f5220e
Sergey Ezdakov <esdakov@mail.ru>
2023-02-23 01:27:40 +0300

    lessson 9 part 1 Form creation

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MyForm from './components/myForm'
+
+function App() {
+  return (
+    <div className="container mt-4">
+      <MyForm />
+    </div>
+  )
+}
+
+export default App
---
+++ src/utils/validateRules.js
@@ -0,0 +1,10 @@
+export default {
+  isRequired: (value) => Boolean(value.trim()),
+  isEmail: (value) => /^\S+@\S+\.\S+$/g.test(value),
+  isUrl: (value) => /^https?:\/\/\S+$/g.test(value),
+  min: (value, length) => value.length >= length,
+  // Содержит заглавную букву
+  isCapitalSymbol: (value) => /[A-Z]+/g.test(value),
+  // Содержит число
+  isContainDigit: (value) => /\d+/g.test(value),
+}
